b97bbea969af1c563089b334dfc6e735062c54e4
Vishal Chaurasiya <vishal8study@gmail.com>
2022-08-21 11:35:43 +0530

    this project is showing how to use Templates

--- storefront/playground/views.py
+++ storefront/playground/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def say_hello(request):
-    return render(request, 'hello.html')
+    return render(request, 'hello.html', { 'name': 'Vishal' })
